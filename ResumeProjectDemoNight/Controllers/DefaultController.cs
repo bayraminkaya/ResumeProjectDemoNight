634f4a95c0d520dfa277dbadd482775bd6fe14c4
bayraminkaya <91468628+bayraminkaya@users.noreply.github.com>
2026-02-02 00:27:48 +0300

    ...

--- ResumeProjectDemoNight/Controllers/DefaultController.cs
+++ ResumeProjectDemoNight/Controllers/DefaultController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ResumeProjectDemoNight.Context;
+using ResumeProjectDemoNight.Entities;
 
 namespace ResumeProjectDemoNight.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly ResumeContext _context;
+
+        public DefaultController(ResumeContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SendMessage(Message message) 
+        {
+            message.IsRead = false;
+            message.SendDate = DateTime.Now;
+            _context.Messages.Add(message);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
