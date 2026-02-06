a744eac0c9cb787298bd1ab05496fb2486b9812e
bayraminkaya <91468628+bayraminkaya@users.noreply.github.com>
2026-02-06 19:06:33 +0300

    ..

---
+++ ResumeProjectDemoNight/Controllers/TestimonialController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResumeProjectDemoNight.Context;
+using ResumeProjectDemoNight.Entities;
+
+namespace ResumeProjectDemoNight.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly ResumeContext _context;
+
+        public TestimonialController(ResumeContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult TestimonialList()
+        {
+            var values = _context.Testimonials.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateTestimonial()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTestimonial(Testimonial testimonial)
+        {
+            _context.Testimonials.Add(testimonial);
+            _context.SaveChanges();
+            TempData["Success"] = "Referans başarıyla eklendi!";
+            return RedirectToAction("TestimonialList");
+        }
+
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            if (value != null)
+            {
+                _context.Testimonials.Remove(value);
+                _context.SaveChanges();
+                TempData["Success"] = "Referans başarıyla silindi!";
+            }
+            return RedirectToAction("TestimonialList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateTestimonial(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            if (value == null)
+            {
+                TempData["Error"] = "Referans bulunamadı!";
+                return RedirectToAction("TestimonialList");
+            }
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            _context.Testimonials.Update(testimonial);
+            _context.SaveChanges();
+            TempData["Success"] = "Referans başarıyla güncellendi!";
+            return RedirectToAction("TestimonialList");
+        }
+    }
+}
